52092d9ea78f8bd2c9188226994c6c0b5e09e9e9
Christopher Robin <williamchen1506@gmail.com>
2025-07-07 08:03:51 +0700

    `feat(pkg) :` add utils left in pkg folder

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,75 @@
+package jwt
+
+import (
+	"time"
+
+	"github.com/CRobinDev/karsa/config/env"
+	"github.com/CRobinDev/karsa/domain/entities"
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	log "github.com/sirupsen/logrus"
+)
+
+var (
+	secretKey   []byte
+	expiredTime time.Duration
+)
+
+func InitJWT() {
+	secretKey = env.GetEnv().JwtSecretKey
+	expiredTime = env.GetEnv().JwtExpiredTime
+}
+
+type Claims struct {
+	Role        string    `json:"role"`
+	UserID      uuid.UUID `json:"user_id"`
+	IsCustomer  bool      `json:"is_customer"`
+	DisplayName string    `json:"username"`
+	jwt.RegisteredClaims
+}
+
+func CreateToken(user *entities.User) (string, error) {
+	claims := Claims{
+		Role:        string(user.Role),
+		UserID:      user.ID,
+		DisplayName: user.Username,
+		IsCustomer:  user.IsCustomer,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(expiredTime)),
+		},
+	}
+	unsignedJWT := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return unsignedJWT.SignedString(secretKey)
+}
+
+func DecodeToken(tokenString string) (Claims, error) {
+	var claims Claims
+	token, err := jwt.ParseWithClaims(tokenString, &claims, func(token *jwt.Token) (any, error) {
+		return secretKey, nil
+	})
+
+	if err != nil {
+		log.Error(map[string]interface{}{
+			"error": err.Error(),
+		}, "[jwt.DecodeToken] failed to parse token")
+		return claims, jwt.ErrTokenExpired
+	}
+
+	if !token.Valid {
+		return claims, jwt.ErrSignatureInvalid
+	}
+
+	return claims, nil
+}
+
+func GetUser(c *fiber.Ctx) (uuid.UUID, error) {
+	claims, ok := c.Locals("userID").(uuid.UUID)
+	if !ok {
+		return uuid.Nil, fiber.NewError(fiber.StatusUnauthorized, "Can't retrieve claims")
+	}
+
+	userID := claims
+
+	return userID, nil
+}
