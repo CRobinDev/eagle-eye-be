786c2dd6afc16d80a9f1f3017f3b52f568b7ac7d
Christopher Robin <williamchen1506@gmail.com>
2025-07-07 08:03:25 +0700

    `feat(logging) :` add logging functionality

---
+++ internal/middleware/logger.go
@@ -0,0 +1,39 @@
+package middleware
+
+import (
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/sirupsen/logrus"
+)
+
+func Logger(logger *logrus.Logger) fiber.Handler {
+	return func(c *fiber.Ctx) error {
+		start := time.Now()
+		err := c.Next()
+		stop := time.Now()
+		latency := stop.Sub(start)
+
+		fields := logrus.Fields{
+			"[ip]":      c.IP(),
+			"[host]":    c.Hostname(),
+			"[url]":     c.OriginalURL(),
+			"[method]":  c.Method(),
+			"[status]":  c.Response().StatusCode(),
+			"[latency]": latency.String(),
+			"[ua]":      c.Get("User-Agent"),
+		}
+
+		status := c.Response().StatusCode()
+		switch {
+		case status >= 500:
+			logger.WithFields(fields).Error("[server error]")
+		case status >= 400:
+			logger.WithFields(fields).Warn("[client error]")
+		default:
+			logger.WithFields(fields).Info("[success]")
+		}
+
+		return err
+	}
+}
---
+++ pkg/log/log.go
@@ -0,0 +1,70 @@
+package log
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/CRobinDev/karsa/config/env"
+	"github.com/google/uuid"
+	"github.com/sirupsen/logrus"
+)
+
+var (
+	logger *logrus.Logger
+	once   sync.Once
+)
+
+func NewLogger() *logrus.Logger {
+	once.Do(func() {
+		logger = logrus.New()
+		logger.SetReportCaller(true)
+
+		logger.SetFormatter(&logrus.TextFormatter{
+			FullTimestamp:   true,
+			TimestampFormat: "Jan 02 15:04:05",
+			CallerPrettyfier: func(f *runtime.Frame) (function string, file string) {
+				repopath, _ := os.Getwd()
+				repopath = filepath.ToSlash(repopath) + "/"
+				filename := strings.Replace(f.File, repopath, " ", 1)
+				return "", fmt.Sprintf("%s:%d", filename, f.Line) 
+			},
+			ForceColors:      true,
+			QuoteEmptyFields: true,
+		})
+
+		repoPath, err := os.Getwd()
+		if err != nil {
+			panic(fmt.Sprintf("failed to get current working directory: %v", err))
+		}
+
+		logFileName := filepath.Join(repoPath, "/storage/logs", fmt.Sprintf("app-%s.log", time.Now().Format("2006-01-02")))
+		file, err := os.OpenFile(logFileName, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+		if err != nil {
+			panic(fmt.Sprintf("failed to open log file: %v", err))
+		}
+
+		multiWriter := io.MultiWriter(os.Stdout, file)
+		logger.SetOutput(multiWriter)
+
+		if env.GetEnv().AppEnv == "production" {
+			logger.SetLevel(logrus.InfoLevel)
+		} else {
+			logger.SetLevel(logrus.DebugLevel)
+		}
+	})
+
+	return logger
+}
+
+func WithTraceID(traceID uuid.UUID, err error) logrus.Fields {
+	return logrus.Fields{
+		"error":    err,
+		"trace_id": traceID,
+	}
+}
