52b8e888d4b13004d8679a51460c8dbdb094d023
Christopher Robin <williamchen1506@gmail.com>
2025-07-13 09:42:43 +0700

    `feat(detection) :` add confidence field

--- database/migration/20250625144943_create_detection.up.sql
+++ database/migration/20250625144943_create_detection.up.sql
@@ -1,12 +1,12 @@
 CREATE TABLE detections (
     id SERIAL NOT NULL PRIMARY KEY, 
-    email VARCHAR(64) NOT NULL,
     ip_address INET NOT NULL,
     customer_id UUID NOT NULL,
     path VARCHAR(128) NOT NULL DEFAULT '',
     method VARCHAR(16) NOT NULL DEFAULT '',
     status_code SMALLINT NOT NULL,
     type VARCHAR(32) NOT NULL default 'unknown',
+    confidence NUMERIC(10,9),
     is_deepfake BOOLEAN NOT NULL DEFAULT false,
     created_at TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
     deleted_at TIMESTAMPTZ NULL DEFAULT NULL,
